bc0bef48b3c3ae1e0a897ab563ddaedf97d9062c
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2026-01-20 12:46:18 +0900

    Add | Geometry Pass

---
+++ TigerEngine/Project/Engine/RenderPass/Renderer/GeometryPass.cpp
@@ -0,0 +1,72 @@
+#include "GeometryPass.h"
+#include "../../Manager/ShaderManager.h"
+
+
+void GeometryPass::Execute(ComPtr<ID3D11DeviceContext>& context, RenderQueue& queue, Camera* cam)
+{
+    auto& sm = ShaderManager::Instance();
+
+    // RTV, DSV
+    ID3D11RenderTargetView* gbuffers[] =
+    {
+        sm.albedoRTV.Get(),
+        sm.normalRTV.Get(),
+        sm.metalRoughRTV.Get(),
+        sm.emissiveRTV.Get()
+    };
+
+    context->RSSetViewports(1, &sm.viewport_screen);
+    context->OMSetRenderTargets(4, gbuffers, sm.depthStencilView.Get());
+    context->ClearDepthStencilView(sm.depthStencilView.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0);
+    context->OMSetDepthStencilState(sm.defualtDSS.Get(), 0);
+
+    for (int i = 0; i < 4; i++)
+    {
+        context->ClearRenderTargetView(gbuffers[i], clearColor);
+    }
+
+    // IA
+    context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    context->IASetInputLayout(sm.inputLayout_Vertex.Get());
+
+    // Shader
+    context->VSSetShader(sm.VS_BaseLit_Model.Get(), NULL, 0);
+    context->PSSetShader(sm.PS_Gbuffer.Get(), NULL, 0);
+
+    // Sampler
+    context->PSSetSamplers(0, 1, sm.linearSamplerState.GetAddressOf());
+
+    // CB
+    sm.transformCBData.view = XMMatrixTranspose(cam->GetView());
+    sm.transformCBData.projection = XMMatrixTranspose(cam->GetProjection());
+    context->UpdateSubresource(sm.transformCB.Get(), 0, nullptr, &sm.transformCBData, 0, 0);
+
+    // Render
+    auto& models = queue.GetSkeletaItems();
+    for (auto& m : models)
+    {
+        // CB - Transform
+        sm.transformCBData.world = m.world.Transpose();
+        sm.transformCBData.isRigid = m.isRigid;
+        sm.transformCBData.refBoneIndex = m.refBoneIndex;
+        context->UpdateSubresource(sm.transformCB.Get(), 0, nullptr, &sm.transformCBData, 0, 0);
+
+        // CB - Offset, Pose
+        auto boneOffset = m.offsets->boneOffset;
+        auto bonePose = m.poses->bonePose;
+
+        for (int i = 0; i < m.boneCount; i++)
+        {
+            sm.offsetMatrixCBData.boneOffset[i] = boneOffset[i].Transpose();
+            sm.poseMatrixCBData.bonePose[i] = bonePose[i].Transpose();
+        }
+        context->UpdateSubresource(sm.offsetMatrixCB.Get(), 0, nullptr, &sm.offsetMatrixCBData, 0, 0);
+        context->UpdateSubresource(sm.poseMatrixCB.Get(), 0, nullptr, &sm.poseMatrixCBData, 0, 0);
+
+        // IB, VB, SRV, CB -> DrawCall
+        m.mesh->Draw(context);
+    }
+
+    // clean up
+    context->OMSetRenderTargets(0, nullptr, nullptr);
+}
---
+++ TigerEngine/Project/Engine/RenderPass/Renderer/GeometryPass.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "../IRenderPass.h"
+#include "pch.h"
+
+/*
+    [ Geometry Renderer ]
+
+     Geometry를 그리기 위한 Stage Setting을 하고,
+     Deferred Rendering을 위해 G-buffer에 라이팅에 필요한 정보를 기록합니다.
+
+
+     ** Geometry Renderer의 Renderable 객체 **
+     Model
+
+     
+     ** G-buffer **
+      RT0 : Albedo (RGB)
+      RT1 : Normal (RGB)
+      RT2 : Metallic (R), Roughness (G)
+      RT3 : Emissive (RGB)
+      ★ Position은 대역폭 절약을 위해 G-buffer에 저장하지 않고,
+        Geometry Pass에서 사용한 뎁스 버퍼를 이용해 Position을 복원해 사용합니다.
+*/
+
+class GeometryPass : public IRenderPass
+{
+private:
+    // g-buffer clear color
+    float clearColor[4] = { 0,0,0,1 };
+
+public:
+    void Init() {};
+    void Execute(ComPtr<ID3D11DeviceContext>& context,
+        RenderQueue& queue, Camera* cam) override;
+    void End(ComPtr<ID3D11DeviceContext>& context) override {}
+};
+
